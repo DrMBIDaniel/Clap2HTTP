039e3f0c5a407ce0b59f0e968e9bba31f34f4714
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-12 20:31:05 +0500

    Update MainActivity.kt

--- app/src/main/java/com/clap2esp/app/MainActivity.kt
+++ app/src/main/java/com/clap2esp/app/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.clap2esp.app
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val microphonePermissionCode = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        checkMicrophonePermission()
+    }
+
+    private fun checkMicrophonePermission() {
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                microphonePermissionCode
+            )
+        }
     }
 }
