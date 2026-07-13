9cbf73dbe17fadf606d8ae9c04964ebe844028e3
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-13 17:44:44 +0500

    Update MainActivity.kt

--- app/src/main/java/com/clap2esp/app/MainActivity.kt
+++ app/src/main/java/com/clap2esp/app/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+       
+        Logger.log("MainActivity created")
 
         checkMicrophonePermission()
 
