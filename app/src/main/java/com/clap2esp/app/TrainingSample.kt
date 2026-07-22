dd1a1bdae8a20951b2c312b03305a798e4f0eb87
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-22 14:41:40 +0500

    Create TrainingSample.kt

---
+++ app/src/main/java/com/clap2esp/app/TrainingSample.kt
@@ -0,0 +1,22 @@
+package com.clap2esp.app
+
+data class TrainingSample(
+
+    val peak: Int,
+
+    val rms: Double,
+
+    val highRatio: Double,
+
+    val zeroCrossings: Int,
+
+    val impulseWidth: Int,
+
+    val spectralCentroid: Double,
+
+    val spectralFlatness: Double,
+
+    val spectralFlux: Double,
+
+    val spectralRollOff: Double
+)
