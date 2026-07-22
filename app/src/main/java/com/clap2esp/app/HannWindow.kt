23c746869ff33e7cb2d563fc9f3ebe55e2df1271
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-22 10:38:44 +0500

    Create HannWindow.kt

---
+++ app/src/main/java/com/clap2esp/app/HannWindow.kt
@@ -0,0 +1,37 @@
+package com.clap2esp.app
+
+import kotlin.math.PI
+import kotlin.math.cos
+
+class HannWindow(
+    private val size: Int
+) {
+
+    private val window = DoubleArray(size)
+
+    init {
+        for (i in 0 until size) {
+            window[i] =
+                0.5 * (
+                    1.0 -
+                    cos(
+                        2.0 * PI * i / (size - 1)
+                    )
+                )
+        }
+    }
+
+    fun apply(input: ShortArray): DoubleArray {
+
+        val output = DoubleArray(size)
+
+        val limit = minOf(input.size, size)
+
+        for (i in 0 until limit) {
+            output[i] =
+                input[i].toDouble() * window[i]
+        }
+
+        return output
+    }
+}
