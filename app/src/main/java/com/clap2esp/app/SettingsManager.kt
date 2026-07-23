203c0c1fa2168e4a0741b6d8b6e943a4ba77d110
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-23 14:19:08 +0500

    Update SettingsManager.kt

--- app/src/main/java/com/clap2esp/app/SettingsManager.kt
+++ app/src/main/java/com/clap2esp/app/SettingsManager.kt
@@ -36,3 +36,29 @@ class SettingsManager(
             .apply()
     }
 }
+   
+    fun saveTrainingData(
+    averagePeak: Double,
+    averageRms: Double,
+    averageHighRatio: Double,
+    averageFlux: Double,
+    averageRollOff: Double,
+    minPeak: Int,
+    maxPeak: Int
+) {
+
+    prefs.edit()
+
+        .putBoolean("trained", true)
+
+        .putFloat("avg_peak", averagePeak.toFloat())
+        .putFloat("avg_rms", averageRms.toFloat())
+        .putFloat("avg_ratio", averageHighRatio.toFloat())
+        .putFloat("avg_flux", averageFlux.toFloat())
+        .putFloat("avg_rolloff", averageRollOff.toFloat())
+
+        .putInt("min_peak", minPeak)
+        .putInt("max_peak", maxPeak)
+
+        .apply()
+}
